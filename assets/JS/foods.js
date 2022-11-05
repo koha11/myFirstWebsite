823b4db815e36178e945bb3bf74a4a6b0bc29cca
koha11 <khoa88108@gmail.com>
2022-11-05 10:01:55 +0700

    Initial commit

---
+++ assets/JS/foods.js
@@ -0,0 +1,84 @@
+export const foodData = [
+  {
+    name: 'Bún thịt luộc',
+    description: 'Bún ăn kèm với thịt luộc, bánh tráng chiên giòn, trứng lòng đào và mọi topping khác nếu thực khách yêu cầu. Có thể ăn với mắm gì cũng được miễn là người ta thích.',
+    img: './assets/img/bún thịt luộc.jpg'
+  },
+  {
+    name: 'Cơm chiên trứng',
+    description: 'Chỉ là cơm chiên với trứng. Đôi khi sẽ có thêm thịt, tốp mỡ, xúc xích cùng nhiều loại topping khác.',
+    img: './assets/img/cơm chiên trứng.JPG'
+  },
+  {
+    name: 'Cơm thịt chiên',
+    description: 'Một bữa ăn đơn giản, cơm trắng ăn cùng thịt chiên với tỏi.',
+    img: './assets/img/cơm thịt chiên.jpg'
+  },
+  {
+    name: 'Gỏi thịt heo chua ngọt',
+    description: 'Một món ăn chuẩn bị rất mất công, mong thực khách suy nghĩ kĩ trước khi lựa chợn.',
+    img: './assets/img/gỏi thịt heo chua ngọt.jpg'
+  },
+  {
+    name: 'Mì sườn ram',
+    description: 'Một món mì đậm đà ăn cùng với nước thịt ram. Thịt có thể là sườn hoặc ba chỉ.',
+    img: './assets/img/mì sườn ram.jpg'
+  },
+  {
+    name: 'Mì Xào lòng gà',
+    description: 'Một món ăn cũng ổn áp nhưng phải nói trước để đầu bếp chuẩn bị chứ không có liền ngay được.',
+    img: './assets/img/mì xào lòng gà.JPG'
+  },
+  {
+    name: 'Mì xốt cà',
+    description: 'Một trong những món xốt cà sở trường, nhưng cũng tùy phong độ, lúc ngon, lúc dở, lúc ngang ngang. Đôi khi chỉ là thịt bằm với cà, có khi còn được thêm xúc xích hoặc là 1 cái đùi gà.',
+    img: './assets/img/mì xốt cà.JPG'
+  },
+  {
+    name: 'Nui xốt cà',
+    description: 'Nui được luộc lên là xào sơ qua với xốt cà. Nhiều lúc sẽ có thêm mấy miếng thịt chiên ăn ngập mặt.',
+    img: './assets/img/nui sốt cà.jpg'
+  },
+  {
+    name: 'Rau củ xào thịt',
+    description: 'Một món ăn khá là healthy.',
+    img: './assets/img/rau củ xào thịt.JPG'
+  },
+  {
+    name: 'Thịt ram cà ri',
+    description: 'Món ăn tự biên tự diễn, vị hơi lạ.',
+    img: './assets/img/thịt ram cà ri.jpg'
+  },
+  {
+    name: 'Thịt bằm cà chua',
+    description: 'Ăn chung với cơm hoặc các loại mỳ.',
+    img: './assets/img/thịt bằm cà chua.jpg'
+  },
+  {
+    name: 'Nói ít hiểu nhiều',
+    description: 'Món ăn như tên gọi.',
+    img: './assets/img/nói ít hiểu nhiều.jpg'
+  },
+  {
+    name: 'Súp gà nấm',
+    description: 'Ăn thì cũng ngon nhưng mà chuẩn bị hơi cực tí.',
+    img: './assets/img/—Pngtree—loading icon vector sign_8367373.png'
+  },
+  {
+    name: 'Thịt ram',
+    description: 'Món tủ, dễ nấu nhất, nấu cũng nhanh nhất, ăn tốn cơm.',
+    img: './assets/img/—Pngtree—loading icon vector sign_8367373.png'
+  },
+  {
+    name: 'Cơm sườn trứng',
+    description: 'Cơm trắng ăn với sườn chiên cùng với 1 quả trứng ốp la, dùng với mắm ngọt hoặc xì dầu tùy thực khách.',
+    img: './assets/img/—Pngtree—loading icon vector sign_8367373.png'
+  },
+  {
+    name: 'Xíu mại',
+    description: 'Có thể là xíu mại hấp, xíu mại sốt cà chua. Có thể ăn chung với cơm hoặc bánh mì. Và sẽ có 1-2 quả trứng ốp la theo yêu cầu của thực khách.',
+    img: './assets/img/—Pngtree—loading icon vector sign_8367373.png'
+  },
+  
+
+]
